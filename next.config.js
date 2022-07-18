a12bf4ec74280d12250c3ff8dcc55ec1451f5ddd
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-07-18 14:30:26 -0300

    chore: add next-pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production';
+
+// const teste = withPWA({
+//   pwa: {
+//     dest: 'public',
+//     disable: !isProd,
+//   },
+// });
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// };
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd,
+  },
+});
